880c09c31d042debc693c5a21f5943c1e5b638b5
andela-fokosun <okosunuzflorence@gmail.com>
2015-12-30 23:17:21 +0100

    [Fixes]Set up testing db

--- tests/TechademiaModelsTest.php
+++ tests/TechademiaModelsTest.php
@@ -1,5 +1,6 @@
 <?php
 
+use Techademia\User;
 use Techademia\Video;
 use Techademia\Category;
 use Illuminate\Foundation\Testing\WithoutMiddleware;
@@ -11,9 +12,9 @@ class ExampleTest extends TestCase
     public function testDatabase()
     {
         Category::create(['title' => 'robotics']);
-        Video::create(['title' => 'cs50']);
+        Video::create(['title' => 'cs50', 'description' => 'Sample description', 'url' => 'https://www.youtube.com/embed/NqVC_4NiAjI', 'user_id' => 1, 'category_id' => 1]);
 
         $this->seeInDatabase('categories', ['title' => 'robotics']);
-        $this->seeInDatabase('videos', ['title' => 'cs50']);
+        $this->seeInDatabase('videos', ['title' => 'cs50', 'description' => 'Sample description', 'url' => 'https://www.youtube.com/embed/NqVC_4NiAjI', 'user_id' => 1, 'category_id' => 1]);
     }
 }
