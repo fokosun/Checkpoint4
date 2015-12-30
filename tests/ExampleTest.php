5bd0494099b45ea4f48f6b511041071a1d3c6d5b
andela-fokosun <okosunuzflorence@gmail.com>
2015-12-30 10:38:28 +0100

    [Fixes]Fix travis build error

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -1,23 +1,12 @@
 <?php
 
-use App\Category;
+use Techademia\Category;
 use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class ExampleTest extends TestCase
 {
-    /**
-     * A basic functional test example.
-     *
-     * @return void
-     */
-    public function testBasicExample()
-    {
-        $this->visit('/')
-             ->see('Laravel 5');
-    }
-
     public function testDatabase()
     {
         Category::create(['title' => 'greece']);
