ca1f41ae2b7080ab7d0956da3bc636855785db8b
andela-fokosun <okosunuzflorence@gmail.com>
2015-12-31 08:56:27 +0100

    [Chores]seperated test cases

--- tests/VideoTest.php
+++ tests/VideoTest.php
@@ -1,20 +1,16 @@
 <?php
 
-use Techademia\User;
 use Techademia\Video;
-use Techademia\Category;
 use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 
-class ExampleTest extends TestCase
+class VideoTest extends TestCase
 {
     public function testDatabase()
     {
-        Category::create(['title' => 'robotics']);
         Video::create(['title' => 'cs50', 'description' => 'Sample description', 'url' => 'https://www.youtube.com/embed/NqVC_4NiAjI', 'user_id' => 1, 'category_id' => 1]);
 
-        $this->seeInDatabase('categories', ['title' => 'robotics']);
         $this->seeInDatabase('videos', ['title' => 'cs50', 'description' => 'Sample description', 'url' => 'https://www.youtube.com/embed/NqVC_4NiAjI', 'user_id' => 1, 'category_id' => 1]);
     }
 }
