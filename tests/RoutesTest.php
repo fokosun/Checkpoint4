d421067b0bdbfae1b9e5e2c8e4722684293d66be
andela-fokosun <okosunuzflorence@gmail.com>
2016-01-01 14:26:51 +0100

    [Feature]unit test invalid routes

--- tests/RoutesTest.php
+++ tests/RoutesTest.php
@@ -1,19 +1,24 @@
 <?php
 
-
-use GuzzleHttp\Client;
-use GuzzleHttp\Exception\ClientException;
 use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class RoutesTest extends TestCase
 {
+
     public function testLandingPage()
     {
         $response = $this->call('GET', '/');
 
         $this->assertEquals(200, $response->status());
+    }
+
+    public function testInvalidEndpoint()
+    {
+        $response = $this->call('GET', '/invalid');
+
+        $this->assertEquals(404, $response->status());
     }
 
 }
