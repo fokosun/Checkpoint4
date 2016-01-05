245c3eadf65fedd31aceafde12fa5f9295153c7b
andela-fokosun <okosunuzflorence@gmail.com>
2016-01-05 16:26:41 +0100

    [Fixes]Fixing appp error on heroku

--- config/database.php
+++ config/database.php
@@ -66,7 +66,7 @@ return [
 
         'pgsql' => [
             'driver'   => 'pgsql',
-            'host'     => env('DB_HOST', 'localhost'),
+            'host'     => env('DB_HOST'),
             'database' => env('DB_DATABASE', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),
             'password' => env('DB_PASSWORD', ''),
