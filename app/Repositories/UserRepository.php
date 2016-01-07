9a2b9d5eb1b4b9b0aef104d468595df1012cc4bd
andela-fokosun <okosunuzflorence@gmail.com>
2016-01-07 12:21:30 +0100

    [Fixes]Debug facebook return values

--- app/Repositories/UserRepository.php
+++ app/Repositories/UserRepository.php
@@ -9,18 +9,19 @@ class UserRepository
     public function findByUserNameOrCreate($userData, $provider)
     {
         $twitter = $provider;
-        $user = $twitter?$this->createByTwitter($userData):$this->createByFaceBookOrGithub($userData);
+        $user = $twitter?$this->createByTwitter($userData, $provider):$this->createByFaceBookOrGithub($userData, $provider);
         $this->checkIfUserNeedsUpdating($userData, $user);
 
         return $user;
     }
 
-    public function createByTwitter($userData)
+    public function createByTwitter($userData, $provider)
     {
         $user = User::where('provider_id', '=', $userData->id)->first();
         if(!$user) {
             $user = User::create([
                 'provider_id' => $userData->id,
+                'provider' => $provider,
                 'fullname' => $userData->name,
                 'username' => $userData->nickname,
                 'avatar' => $userData->avatar,
@@ -30,15 +31,15 @@ class UserRepository
         return $user;
     }
 
-    public function createByFacebookOrGithub()
+    public function createByFacebookOrGithub($userData, $provider)
     {
-        dd($userData->email . ' ' . $userData->name);
         $user = User::where('provider_id', '=', $userData->id)->first();
         if(!$user) {
             $user = User::create([
                 'provider_id' => $userData->id,
+                'provider' => $provider,
                 'fullname' => $userData->name,
-                'username' => $userData->name,
+                'username' => $userData->nickname,
                 'email' => $userData->email,
                 'avatar' => $userData->avatar,
             ]);
