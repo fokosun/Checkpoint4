6ecf3ad25101a5506df40bd7297edb2288e593d0
andela-fokosun <okosunuzflorence@gmail.com>
2016-01-07 19:07:31 +0100

    [TEST]

--- tests/RoutesTest.php
+++ tests/RoutesTest.php
@@ -11,4 +11,28 @@ class RoutesTest extends TestCase
         $response = $this->call('GET', '/');
         $this->assertEquals(200, $response->status());
     }
+
+    public function testUserLogin()
+    {
+        Session::start();
+        $params = [
+            '_token' => csrf_token(),
+            'email'     => 'florence.okosun@andela.com',
+            'password'  => 'pass'
+        ];
+
+        $response = $this->call('POST', '/auth/login', $params);
+        $this->assertEquals(302, $response->status());
+    }
+
+    public function testUserLogout()
+    {
+        Session::start();
+        $params = [
+            '_token' => csrf_token()
+        ];
+
+        $response = $this->call('GET', '/auth/logout', $params);
+        $this->assertEquals(302, $response->status());
+    }
 }
