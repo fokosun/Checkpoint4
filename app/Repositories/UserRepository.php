edd1c2178d12a14a37b2bb57afab70b5ee6b6e3d
andela-fokosun <okosunuzflorence@gmail.com>
2016-01-11 16:20:21 +0100

    [fixes]fix fb social auth issues

--- app/Repositories/UserRepository.php
+++ app/Repositories/UserRepository.php
@@ -43,15 +43,7 @@ class UserRepository
 
     public function facebook($userData, $provider)
     {
-        $user = User::where('provider_id', '=', $userData->id)->first();
-        // dd($user);
-        if(!$user) {
-            $user = User::create([
-                'fullname' => $userData->getName()
-            ]);
-        }
-
-        return $user;
+        dd($userData);
     }
 
     public function github($userData, $provider)
