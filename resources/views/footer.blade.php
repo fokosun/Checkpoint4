f03412125959517eab04d2afb3d3b02e1f9f80bf
andela-fokosun <okosunuzflorence@gmail.com>
2016-01-25 08:55:42 +0000

    [Fixes]Add correct text to footer and corrected typo in readme

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,7 +1,6 @@
 <footer class="main-footer center">
     <div class="pull-right hidden-xs">
-        <b>Version</b> 1.0
+        <b>Version</b> 1.0 | Copyright © 2016  All rights reserved.
     </div>
-    <strong>Copyright © 2016 <a href="http://www.andela.com/">Andela</a>.</strong> All rights
-    reserved.
+    <strong>Made with <span class="text-red"><i class="fa fa-heart"></i></span> <a href="https://github.com/andela-fokosun"> by Florence Okosun</a> from <a href="http://www.andela.com/">Andela. </a></strong>
 </footer>
