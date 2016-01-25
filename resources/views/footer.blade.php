8d5efd3783df2359724e4d419109a8d95eca1e93
andela-fokosun <okosunuzflorence@gmail.com>
2016-01-25 09:02:49 +0000

    [Chore]Fix typo error in footer credits

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -2,5 +2,5 @@
     <div class="pull-right hidden-xs">
         <b>Version</b> 1.0 | Copyright © 2016  All rights reserved.
     </div>
-    <strong>Made with <span class="text-red"><i class="fa fa-heart"></i></span> <a href="https://github.com/andela-fokosun"> by Florence Okosun</a> from <a href="http://www.andela.com/">Andela. </a></strong>
+    <strong>Made with by<span class="text-red"><i class="fa fa-heart"></i></span> <a href="https://github.com/andela-fokosun"> Florence Okosun</a> from <a href="http://www.andela.com/">Andela. </a></strong>
 </footer>
