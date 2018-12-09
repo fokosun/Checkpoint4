d04182d766f500b990d869e3ad9658f2b817d3a2
fokosun <okosunuzflorence@gmail.com>
2018-12-09 23:50:23 +0100

    provide video link

--- resources/views/pages/alert.blade.php
+++ resources/views/pages/alert.blade.php
@@ -13,7 +13,7 @@
         <div class="alert alert-success alert-dismissible">
             <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
             <h4><i class="icon fa fa-check"></i> Success!</h4>
-            {{ session()->get('status')}}
+            Check out your new video <a href="{{ session()->get('status') }}">here</a>
         </div>
     </div>
 @elseif ( session()->has('warning'))
