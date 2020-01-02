7e3851215115fa59be15be90fbead66566506fae
fokosun <okosunuzflorence@gmail.com>
2020-01-01 18:05:31 -0700

    chore

--- resources/views/pages/alert.blade.php
+++ resources/views/pages/alert.blade.php
@@ -19,7 +19,7 @@
 {{--        </div>--}}
 {{--    </div>--}}
 {{--@else--}}
-@if ( session()->has('warning'))
+@elseif ( session()->has('warning'))
 <div class="row">
         <div class="alert alert-warning alert-dismissible">
             <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
