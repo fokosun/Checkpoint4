2481a1e8d935f895703780dde885a7a47118532f
fokosun <okosunuzflorence@gmail.com>
2020-01-01 17:59:20 -0700

    disabling profile page for now

--- resources/views/pages/alert.blade.php
+++ resources/views/pages/alert.blade.php
@@ -3,22 +3,23 @@
     <div class="row">
         <div class="alert alert-danger alert-dismissible">
             <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-            <h4><i class="icon fa fa-check"></i> Form contains errors</h4>
+            <h4><i class="icon fa fa-check"></i> This form contains errors</h4>
             @foreach($errors->all() as $error)
             <p>{{ $error }}</p>
             @endforeach
         </div>
     </div>
 
-@elseif ( session()->has('status'))
-    <div class="row">
-        <div class="alert alert-success alert-dismissible">
-            <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-            <h4><i class="icon fa fa-check"></i> Success!</h4>
-            Check out your new video <a href="{{ session()->get('status') }}">here</a>
-        </div>
-    </div>
-@elseif ( session()->has('warning'))
+{{--@elseif ( session()->has('status'))--}}
+{{--    <div class="row">--}}
+{{--        <div class="alert alert-success alert-dismissible">--}}
+{{--            <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>--}}
+{{--            <h4><i class="icon fa fa-check"></i> Success!</h4>--}}
+{{--            Check out your new video <a href="{{ session()->get('status') }}">here</a>--}}
+{{--        </div>--}}
+{{--    </div>--}}
+{{--@else--}}
+@if ( session()->has('warning'))
 <div class="row">
         <div class="alert alert-warning alert-dismissible">
             <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
